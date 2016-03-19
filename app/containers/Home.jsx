d94cae20bc2e68ad06b4db7da405cea5d1033a9b
Developver <paulxuDev@Pauls-MacBook-Air.local>
2016-03-19 18:25:52 -0400

    First

---
+++ app/containers/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+export default class Home extends Component{
+	render(){
+		return(<h1>Hello!</h1>)
+	}
+}
---
+++ app/routes.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from 'containers/App';
+import Vote from 'containers/Vote';
+import About from 'containers/About';
+import LoginOrRegister from 'containers/LoginOrRegister';
+import Dashboard from 'containers/Dashboard';
+import Home from 'containers/Home';
+
+/*
+ * @param {Redux Store}
+ * We require store as an argument here because we wish to get
+ * state from the store after it has been authenticated.
+ */
+export default (store) => {
+  const requireAuth = (nextState, replace, callback) => {
+    const { user: { authenticated }} = store.getState();
+    if (!authenticated) {
+      replace({
+        pathname: '/login',
+        state: { nextPathname: nextState.location.pathname }
+      });
+    }
+    callback();
+  };
+
+  const redirectAuth = (nextState, replace, callback) => {
+    const { user: { authenticated }} = store.getState();
+    if (authenticated) {
+      replace({
+        pathname: '/'
+      });
+    }
+    callback();
+  };
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+    </Route>
+  );
+};
